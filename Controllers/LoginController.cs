9a4fcd737d41fc2d356dbbbed6ef4917b61b4739
FIC <FIC@info.local>
2021-01-28 10:13:36 -0300

    Atualizações:Models e Controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_Instadev.Controllers
+{
+    [Route("Login")]
+    public class LoginController: Controller
+    {
+        
+        [TempData]
+         public string Mensagem { get; set; }
+
+        public IActionResult Logar(IFormCollection form)
+        {
+            Usuario usuarioModel = new Usuario();
+            // Lemos todos os arquivos do CSV
+
+            List<string> csv = jogadorModel.ReadAllLinesCSV("Database/Jogador.csv");
+
+            // Verificamos se as informações passadas existe na lista de string
+            var logado = 
+            csv.Find(
+                x => 
+                x.Split(";")[2] == form["Email"] && 
+                x.Split(";")[3] == form["Senha"]
+            );
+
+            
+            
+            if(logado != null)
+            {
+                return LocalRedirect("~/");
+            }else{
+
+            Mensagem = "Dados incorretos, tente novamente...";
+            return LocalRedirect("~/Login");
+            }
+
+        }
+    }
+}
+
+    
