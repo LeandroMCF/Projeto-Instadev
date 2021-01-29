3a8e16d0c6be950a32754b4e8db65d0c9b4bdf02
Prado-SCamila <prado.camila.910423@gmail.com>
2021-01-29 00:26:46 -0300

    Login Controller Finalizado

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,42 +3,59 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Projeto_Instadev.Models;
 
+
 namespace Projeto_Instadev.Controllers
 {
+
     [Route("Login")]
+
     public class LoginController: Controller
     {
-        
         [TempData]
-         public string Mensagem { get; set; }
+        public string Mensagem { get; set; }
+        Usuario usuarioModel = new Usuario();
 
+        public IActionResult Index()
+    {
+        return View();
+    }
+        
+
+       [Route("Logar")]
+      
         public IActionResult Logar(IFormCollection form)
         {
+             
             Usuario usuarioModel = new Usuario();
-            // Lemos todos os arquivos do CSV
-
+            
             List<string> csv = usuarioModel.ReadAllLinesCSV("Database/Usuario.csv");
 
-            // Verificamos se as informações passadas existe na lista de string
             var logado = 
             csv.Find(
                 x => 
                 x.Split(";")[2] == form["Email"] && 
                 x.Split(";")[3] == form["Senha"]
             );
-
-            
+                    
             if(logado != null)
             {
+                 HttpContext.Session.SetString("_UserName", logado.Split(";")[1]);
                 return LocalRedirect("~/");
             }else{
-
-            Mensagem = "Dados incorretos, tente novamente...";
-
+           
+             Mensagem = "Senha ou username incorreto !";
+            
             return LocalRedirect("~/Login");
             }
 
         }
+
+            [Route("Logout")]
+            public IActionResult Logout()
+            {
+                 HttpContext.Session.Remove("_UserName");
+                 return LocalRedirect("~/");
+            }
     }
 }
 
